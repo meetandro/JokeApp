cd8680c7530c6ee29ad5f1abc1292239113a03aa
Andro Bakashvili <bakashviliandro@gmail.com>
2024-05-31 23:58:18 +0400

    Update appsettings.json, migrations

--- JokeApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ JokeApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace JokeApp.Data.Migrations
+namespace JokeApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
